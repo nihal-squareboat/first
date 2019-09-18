17476189e5bfac7c2ecec19590c0f7f65733cca3
Nihal Arya <nihal.arya@squareboat.com>
2019-09-18 20:45:09 +0530

    autoComplete

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,16 @@
 @extends('layouts.app')
-@section('content')
+
 @if(Auth::user()->usertype=='admin')
-    
-    <script>window.location = "/nova";</script>
+    @section('content')
+        <script>window.location = "/nova";</script>
     @endsection
 @else
     @include(Auth::user()->usertype)
     @section('content')
+        @if(Auth::user()->email_verified_at==null)
+            <h2 style="text-align: center;">Kindly verify mail to continue</h2>
+        @else
             @yield(Auth::user()->usertype)
+        @endif
     @endsection
 @endif
