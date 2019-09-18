c293d9b9d9fa537be4aa90c4e7a11b5a038a38cb
Nihal Arya <nihal.arya@squareboat.com>
2019-09-18 20:24:01 +0530

    autoComplete

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,7 +43,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" minlength="6"  maxlength="150" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <input id="password" autocomplete="off" value="" minlength="6"  maxlength="150" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
