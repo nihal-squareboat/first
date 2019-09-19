f3b0d70c8c78b5c94bcd50d2d5ec099fcd34b594
Nihal Arya <nihal.arya@squareboat.com>
2019-09-19 10:07:06 +0530

    mail resend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,26 @@
     @include(Auth::user()->usertype)
     @section('content')
         @if(Auth::user()->email_verified_at==null)
-            <h2 style="text-align: center;">Kindly verify mail to continue</h2>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+        
+                        <div class="card-body">
+                            @if (session('resent'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ __('A fresh verification link has been sent to your email address.') }}
+                                </div>
+                            @endif
+        
+                            {{ __('Before proceeding, please check your email for a verification link.') }}
+                            {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         @else
             @yield(Auth::user()->usertype)
         @endif
